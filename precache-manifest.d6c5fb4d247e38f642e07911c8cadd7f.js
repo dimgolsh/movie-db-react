d35592c0b68d3dfa56c47178a410ab6ad010b3b4
dimon <dimagol64@gmail.com>
2020-04-17 18:25:57 +0300

    Updates

---
+++ precache-manifest.d6c5fb4d247e38f642e07911c8cadd7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "796259af021063a07376d18ea138bd5f",
+    "url": "/movie-db-react/index.html"
+  },
+  {
+    "revision": "e3433114a7e6477e1b4f",
+    "url": "/movie-db-react/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "5213730d5544e10bfe0f",
+    "url": "/movie-db-react/static/css/main.5d937b94.chunk.css"
+  },
+  {
+    "revision": "e3433114a7e6477e1b4f",
+    "url": "/movie-db-react/static/js/2.186c8d0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-db-react/static/js/2.186c8d0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5213730d5544e10bfe0f",
+    "url": "/movie-db-react/static/js/main.5cf840fc.chunk.js"
+  },
+  {
+    "revision": "20ce13668b181ca0b4b9",
+    "url": "/movie-db-react/static/js/runtime-main.4bd30999.js"
+  }
+]);
